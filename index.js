96ed7209b7c7dcd1553e00477b4a6691991ac7a8
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-04-14 19:51:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const controller = require('./controller')
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', controller.createUser)
+app.get('/api/users', controller.getUser)
 
 
 
