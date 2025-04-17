100f467b9027ce4789af035a341c8c355d526c65
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2025-04-17 20:56:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const controller = require('./controller')
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 app.post('/api/users', controller.createUser)
-app.get('/api/users', controller.getUser)
-
+app.get('/api/users', controller.findAllUsers)
+app.post('/api/users/:_id/exercises', controller.createExercise)
+app.get('/api/users/:_id/logs',controller.getAllLogs)
 
 
 
